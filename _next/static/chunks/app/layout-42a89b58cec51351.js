0c308e8a2dd1518d60495e70c9605512c5076081
Simon Socolow <simonsocolow78@gmail.com>
2025-07-02 20:28:37 -0400

    Updates

--- _next/static/chunks/app/layout-42a89b58cec51351.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},2387:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,8346,23)),Promise.resolve().then(a.t.bind(a,347,23))},8346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_59ea64"}}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,684,358],()=>s(2387)),_N_E=e.O()}]);
